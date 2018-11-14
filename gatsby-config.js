b024b12138a7bf68cf80b1bb03c47660d3f220a7
Maryna Karpovets <Maryna_Karpovets@epam.com>
2018-11-14 16:29:41 +0200

    change config

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
-        path: `${__dirname}/src/pages`
+        path: `${__dirname}/src`
       }
     },
     `gatsby-plugin-styled-components`
